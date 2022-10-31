da2d4ac5f57b6e43a96245fbb2be2a96a6a8338c
higracehuang <117108383+higracehuang@users.noreply.github.com>
2022-10-31 12:23:53 -0500

    Initial Commit

---
+++ EasyScreenshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyScreenshot
+//
+//  Created by Le Huang on 10/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
